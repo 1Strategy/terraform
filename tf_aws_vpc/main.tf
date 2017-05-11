da2a1271d2102781b37e3dfdbce5d6a67a437fd0
alex <alex@1strategy.com>
2017-05-11 15:06:56 -0600

    Terraform plan works at this commit. Applied and destroyed tf_aws_vpc/main.tf.

--- tf_aws_vpc/main.tf
+++ tf_aws_vpc/main.tf
@@ -1,3 +1,10 @@
+# Establish provider
+provider "aws" {
+  region = "us-west-2"
+  profile = "sandbox"
+}
+
+# Create resources
 module "instance" {
   source = "github.com/1Strategy/terraform//tf_aws_modules/"
 }
