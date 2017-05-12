04c5378b03c5cc78d0d0feeedbdad121a587c15e
alex <alex@1strategy.com>
2017-05-12 16:37:43 -0600

    move variables into main.tf

--- tf_aws_modules/main.tf
+++ tf_aws_modules/main.tf
@@ -9,3 +9,23 @@ resource "aws_instance" "test_instance" {
     Name = "HelloWorld"
   }
 }
+
+################################################################################
+# VARIABLES
+################################################################################
+
+variable "startup_script" {
+  description = "A script to be run when the instance is spun up; the User Data."
+}
+
+variable "ssh_key" {
+  description = "The key pair that will be used to ssh into this instance."
+}
+
+################################################################################
+# OUTPUTS
+################################################################################
+
+# output "ec2_instance_id" {
+#   value = "${aws_instance.ec2_instance.id}"
+# }
