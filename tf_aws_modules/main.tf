9f008a205674796fdb008d2ab45497e9e177a3f7
alex <alex@1strategy.com>
2017-05-12 16:31:45 -0600

    reorganize instance module

--- tf_aws_modules/main.tf
+++ tf_aws_modules/main.tf
@@ -3,12 +3,9 @@
 resource "aws_instance" "test_instance" {
   ami           = "ami-4836a428"
   instance_type = "t2.micro"
-
+  user_data     = "${var.startup_script}"
+  key_name      = "${var.ssh_key}"
   tags {
     Name = "HelloWorld"
   }
-
-  user_data = "${var.startup_script}"
-
-  key_name = "${var.ssh_key}"
 }
