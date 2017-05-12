f1b8b0dde686cba96595757c4ac203badde098ab
alex <alex@1strategy.com>
2017-05-12 16:21:54 -0600

    add key to ssh into the test instance

--- tf_aws_vpc/main.tf
+++ tf_aws_vpc/main.tf
@@ -27,10 +27,10 @@ terraform {
 
 
 # Create resources
-module "instance" {
+module "test_instance" {
   source = "github.com/1Strategy/terraform//tf_aws_modules/"
-
-#   startup_script = <<EOF
-# sudo apt-get update -y
-# EOF
+  ssh_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCGj9TwDOk37yXBKYkM3FIe+nzrs+WHv+8O0G9iMSagUBZPkTRIU4ksNQQYlaSPF3G4BJEf3U/i1vrLjMwwSdyZunCF2dLPyo5Qlar9dhiGkFEl+mbeLNmGWcueLxd4wwz66Wq2xtJVZkz4TAxzYIFAB2ovCVuz29mW8YZh4UoNWci7e5AsE4MXtYf0MibV+ENZQwcZpnQi1KL4r54DeCGGCYYfm6H9QcAK48zzr7IRaQngSU0Rq7G0gaak44IR7EXvz+AeL8mSAjh/rj5o3Nfq2U4Caed3DpgQMW0gX2sM480l/PcuexLBnfOTDxNLA4p6weetak9itT3mRPmUpj9H"
+  startup_script = <<EOF
+sudo apt-get update -y
+EOF
 }
