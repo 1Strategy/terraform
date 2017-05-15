c4dc2b19717348750efd237dca944f9c51755fca
alex <alex@1strategy.com>
2017-05-15 17:10:37 -0600

    delete ssh_key variables

--- modules/main.tf
+++ modules/main.tf
@@ -4,7 +4,6 @@ resource "aws_instance" "test_instance" {
   ami           = "ami-4836a428"
   instance_type = "t2.micro"
   user_data     = "${var.startup_script}"
-  key_name      = "${var.ssh_key}"
   tags {
     Name = "HelloWorld"
   }
